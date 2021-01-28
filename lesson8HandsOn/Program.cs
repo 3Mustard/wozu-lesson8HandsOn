a711d003722c98bdd578adaa6a0ce3aa9a4a32e6
Matthew Cullen <m23.cullen@gmail.com>
2021-01-28 17:06:55 -0500

    finish both parts

--- lesson8HandsOn/Program.cs
+++ lesson8HandsOn/Program.cs
@@ -4,28 +4,49 @@ namespace lesson8HandsOn
 {
     class Program
     {
+        // MAIN METHOD
         static void Main(string[] args)
         {
+            // declare variables and instantiate new objects
             Dog d1 = new Dog();
             Cat c1 = new Cat();
             Cow cow1 = new Cow();
 
+            // use methods inherited by interfaces
             d1.Speak();
             d1.Run();
+            d1.Eat();
             c1.Speak();
             c1.Run();
+            c1.Eat();
             cow1.Speak();
             cow1.Run();
+            cow1.Eat();
         }
+        // INTERFACES
 
+        public interface IAnimal
+        {
+            void Eat();
+        }
         public interface IMammel
+            : IAnimal
         {
             void Speak();
             void Run();
         }
 
-        public class Dog : IMammel
-        { 
+        // CLASSES
+
+        // DOG
+        public class Dog 
+            : IMammel
+        {
+            public void Eat()
+            {
+                Console.WriteLine("Dogs eat bones.");
+            }
+
             public void Speak()
             {
                 Console.WriteLine("Bark!");
@@ -36,8 +57,15 @@ namespace lesson8HandsOn
                 Console.WriteLine("Dogs can run at a top speed of 45 mph!");
             }
         }
-        public class Cat : IMammel
+        // CAT
+        public class Cat 
+            : IMammel
         {
+            public void Eat()
+            {
+                Console.WriteLine("Cats eat mice.");
+            }
+
             public void Speak()
             {
                 Console.WriteLine("Meow!");
@@ -48,8 +76,15 @@ namespace lesson8HandsOn
                 Console.WriteLine("Cats can run at a top speed of 30 mph!");
             }
         }
-        public class Cow : IMammel
+        // Cow
+        public class Cow 
+            : IMammel
         {
+            public void Eat()
+            {
+                Console.WriteLine("Cows eat grass.");
+            }
+
             public void Speak()
             {
                 Console.WriteLine("Moo!");
