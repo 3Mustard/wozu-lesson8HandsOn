bdf3f002cc8b21b16539f797ee6e5ca7a4abe81f
Matthew Cullen <m23.cullen@gmail.com>
2021-01-28 16:59:56 -0500

    first commit. solved part 1

---
+++ lesson8HandsOn/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace lesson8HandsOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d1 = new Dog();
+            Cat c1 = new Cat();
+            Cow cow1 = new Cow();
+
+            d1.Speak();
+            d1.Run();
+            c1.Speak();
+            c1.Run();
+            cow1.Speak();
+            cow1.Run();
+        }
+
+        public interface IMammel
+        {
+            void Speak();
+            void Run();
+        }
+
+        public class Dog : IMammel
+        { 
+            public void Speak()
+            {
+                Console.WriteLine("Bark!");
+            }
+
+            public void Run()
+            {
+                Console.WriteLine("Dogs can run at a top speed of 45 mph!");
+            }
+        }
+        public class Cat : IMammel
+        {
+            public void Speak()
+            {
+                Console.WriteLine("Meow!");
+            }
+
+            public void Run()
+            {
+                Console.WriteLine("Cats can run at a top speed of 30 mph!");
+            }
+        }
+        public class Cow : IMammel
+        {
+            public void Speak()
+            {
+                Console.WriteLine("Moo!");
+            }
+
+            public void Run()
+            {
+                Console.WriteLine("Cows can run at a top speed of 25 mph!");
+            }
+        }
+    }
+}
